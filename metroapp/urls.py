2cdb6b865ea5837e23c81359c026459d9b7eab51
Tristan Guigue <tristan.guigue@gmail.com>
2017-12-03 20:17:40 +0000

    Remove contacts

--- metroapp/urls.py
+++ metroapp/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import url
 from django.views.i18n import JavaScriptCatalog
 
-from .views import ContactView, ExplanationView, HomeView, MethodologyView, RelevanceView
+from .views import ExplanationView, HomeView, MethodologyView, RelevanceView
 from .views import set_language_from_url
 
 urlpatterns = [
@@ -10,7 +10,6 @@ urlpatterns = [
     url(r'^methodology', MethodologyView.as_view(), name='methodology-view'),
     url(r'^relevance', RelevanceView.as_view(), name='relevance-view'),
     url(r'^explanation', ExplanationView.as_view(), name='explanation-view'),
-    url(r'^contact', ContactView.as_view(), name='contact-view'),
     url(r'^setlang/(?P<user_language>\w+)/$', set_language_from_url, name='set_language_from_url'),
     url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 ]
